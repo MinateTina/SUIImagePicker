cbbad364deeaa48e706c00e56628e84ad673b28e
MinateTina <tinatung509@gmail.com>
2022-10-03 11:58:10 -0700

    Initial Commit

---
+++ SUIImagePicker/SUIImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIImagePickerApp.swift
+//  SUIImagePicker
+//
+//  Created by Minate on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SUIImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
